cf6b77b98fc344b90707dcc8dd9c014fddf02b0f
Michal Wojciechowski <odyniec@cpan.org>
2014-10-09 19:58:52 +0200

    Add a method to check if a comment looks like spam

--- lib/Dancer/Plugin/Commentary/Feature/Akismet.pm
+++ lib/Dancer/Plugin/Commentary/Feature/Akismet.pm
@@ -20,4 +20,19 @@ sub new {
     return bless $self, $class;
 }
 
+sub is_spam {
+    my ($self, $args) = @_;
+
+    my $result = $self->{_akismet}->check(
+        %{
+            map { uc $_ => $args->{$_} }
+                grep { exists $args->{$_} }
+                    qw( comment_author comment_author_email comment_content
+                        comment_user_agent referrer user_ip );
+        }
+    );
+
+    return { true => 1, false => 0 }->{$result};
+}
+
 1;
