85908bcc354fc821ed62934c9fe2bd3eeb9e3992
Michal Wojciechowski <odyniec@cpan.org>
2014-10-14 19:03:23 +0200

    Add a to_html method to Markdown format class

--- lib/Dancer/Plugin/Commentary/Format/Markdown.pm
+++ lib/Dancer/Plugin/Commentary/Format/Markdown.pm
@@ -3,6 +3,14 @@ package Dancer::Plugin::Commentary::Format::Markdown;
 use strict;
 use warnings;
 
+use Text::Markdown;
+
 use parent 'Dancer::Plugin::Commentary::Format';
+
+sub to_html {
+    my ($text) = @_;
+
+    return Text::Markdown::markdown($text);
+}
 
 1;
