35d5e9fb4998efcec22e17b42860d0d8c8638cdc
Michal Wojciechowski <odyniec@cpan.org>
2014-10-15 23:56:22 +0200

    Add class for the basic format

---
+++ lib/Dancer/Plugin/Commentary/Format/Basic.pm
@@ -0,0 +1,13 @@
+package Dancer::Plugin::Commentary::Format::Basic;
+
+use strict;
+use warnings;
+
+use parent 'Dancer::Plugin::Commentary::Format';
+
+sub to_html {
+    my ($text) = @_;
+
+}
+
+1;
