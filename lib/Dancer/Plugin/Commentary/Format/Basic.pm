6f348b8bd941f4b3fb889250748033567c60b650
Michal Wojciechowski <odyniec@cpan.org>
2014-10-16 23:39:50 +0200

    Replace URLs with anchors for basic format

--- lib/Dancer/Plugin/Commentary/Format/Basic.pm
+++ lib/Dancer/Plugin/Commentary/Format/Basic.pm
@@ -8,6 +8,14 @@ use parent 'Dancer::Plugin::Commentary::Format';
 sub to_html {
     my ($text) = @_;
 
+    # Replace text URLs with anchors (monstrous regexp borrowed from URI::Find)
+    $text =~ s{
+        (?^:[a-zA-Z][a-zA-Z0-9\+]*):
+        [\;\/\?\\@\&\=\+\$\,\[\]\p{isAlpha}A-Za-z0-9\-_\.\!\~\*\'\(\)%]
+        [\;\/\?\:\@\&\=\+\$\,\[\]\p{isAlpha}A-Za-z0-9\-_\.\!\~\*\'\(\)%\#]*
+    }{<a href="$&">$&</a>}gsx;
+
+    return $text;
 }
 
 1;
