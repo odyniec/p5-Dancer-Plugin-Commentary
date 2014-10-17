431a10d7296f8001202ccae92e1e939977d60d38
Michal Wojciechowski <odyniec@cpan.org>
2014-10-17 21:11:59 +0200

    Make paragraphs and strip leading/trailing whitespace in basic format

--- lib/Dancer/Plugin/Commentary/Format/Basic.pm
+++ lib/Dancer/Plugin/Commentary/Format/Basic.pm
@@ -8,12 +8,19 @@ use parent 'Dancer::Plugin::Commentary::Format';
 sub to_html {
     my ($text) = @_;
 
+    # Strip leading and trailing whitespace
+    $text =~ s{^\s+|\s+$}{}gs;
+
     # Replace text URLs with anchors (monstrous regexp borrowed from URI::Find)
     $text =~ s{
         (?^:[a-zA-Z][a-zA-Z0-9\+]*):
         [\;\/\?\\@\&\=\+\$\,\[\]\p{isAlpha}A-Za-z0-9\-_\.\!\~\*\'\(\)%]
         [\;\/\?\:\@\&\=\+\$\,\[\]\p{isAlpha}A-Za-z0-9\-_\.\!\~\*\'\(\)%\#]*
     }{<a href="$&">$&</a>}gsx;
+
+    # Make paragraphs
+    $text =~ s{\n\n}{</p><p>}gs;
+    $text = "<p>$text</p>";
 
     return $text;
 }
