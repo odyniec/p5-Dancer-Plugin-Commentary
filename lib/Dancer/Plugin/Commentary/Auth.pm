82297b74efeb013e162b1eea05e4bb6e5bd6ef0b
Michal Wojciechowski <odyniec@cpan.org>
2014-10-18 16:21:07 +0200

    Add method to get current authentication data

--- lib/Dancer/Plugin/Commentary/Auth.pm
+++ lib/Dancer/Plugin/Commentary/Auth.pm
@@ -11,4 +11,18 @@ sub authentication_url { }
 
 sub auth_data { }
 
+sub current_method_data {
+    my ($class) = @_;
+
+    for my $method (values %methods) {
+        my $method_data = $method->method_data;
+
+        if ($method_data->{authenticated}) {
+            return $method_data;
+        }
+    }
+
+    return;
+}
+
 1;
