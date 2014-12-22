9cee4787b056c34c80d07a9bae790e87fc7d6776
Michal Wojciechowski <odyniec@cpan.org>
2014-12-22 11:42:05 +0100

    Use an up-to-date version of ago()

--- share/assets/js/commentary.js
+++ share/assets/js/commentary.js
@@ -27,15 +27,15 @@ function addPrerequisite(prerequisite) {
     prerequisites[prerequisites.length] = prerequisite;
 }
 
-function ago(v, f) {
-    v = ~~((Date.now() - v)/1e3);
-    var a, b = { second: 60, minute: 60, hour: 24, day: 7, week: 4.35,
-        month: 12, year: 1e4 }, c;
+function ago(val) {
+    val = 0 | (Date.now() - val) / 1000;
+    var unit, length = { second: 60, minute: 60, hour: 24, day: 7, week: 4.35,
+        month: 12, year: 10000 }, result;
  
-    for (a in b) {
-        c=v % b[a];
-        if (!(v = (f||parseInt)(v / b[a])))
-            return c + ' ' + (c-1 ? a + 's' : a);
+    for (unit in length) {
+        result = val % length[unit];
+        if (!(val = 0 | val / length[unit]))
+            return result + ' ' + (result-1 ? unit + 's' : unit);
     }
 }
 
